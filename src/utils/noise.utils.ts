edab20fdfac809f868a9c00154ad3ae9abae57a4
Eperson Mayrink <epersoncmxf@gmail.com>
2025-09-20 12:30:04 -0300

    fix

--- src/utils/noise.utils.ts
+++ src/utils/noise.utils.ts
@@ -1,25 +1,23 @@
 // noise.utils.ts
-import Noise from 'noisejs';
+import { makeNoise2D } from 'open-simplex-noise';
 
 export class NoiseUtils {
   private static seed = 0;
-  private static noise = new Noise(0);
+  private static noise2D = makeNoise2D(0);
 
   static setSeed(seed: number) {
     this.seed = seed;
-    this.noise = new Noise(seed);
+    this.noise2D = makeNoise2D(seed);
   }
 
   static fbm2(x: number, y: number, octaves = 4, seed?: number) {
-    if (seed !== undefined && seed !== this.seed) {
-      this.setSeed(seed);
-    }
+    if (seed !== undefined && seed !== this.seed) this.setSeed(seed);
     let v = 0, amp = 0.5, sumAmp = 0;
     for (let i = 0; i < octaves; i++) {
-      v += amp * this.noise.perlin2(x, y);
+      v += amp * this.noise2D(x, y);
       sumAmp += amp;
       x *= 2; y *= 2; amp *= 0.5;
     }
-    return (v / sumAmp) * 0.5 + 0.5;
+    return (v / sumAmp) * 0.5 + 0.5; // [0,1]
   }
 }
